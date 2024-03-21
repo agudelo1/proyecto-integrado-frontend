7aaa7e3ab0a88a215f5f48912954bd2b11ec8bd4
Cristian Alberto Agudelo Marquez <cristian.agudelo1@udea.edu.co>
2024-03-20 20:15:27 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import { FaRegUser, FaRegEnvelope } from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
+      <head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
+          <div className="w-3/5 p-5">
+            <div className="text-left font-bold text-2xl">
+              <span className="text-green-500">Deli</span>Dash
+            </div>
+            <div className="py-10">
+              <h2 className="text-3xl font-bold text-green-500 mb-2">
+                Sign in to Account
+              </h2>
+              <div className="border-2 w-10 border-green-500 inline-block mb-2"></div>
+              <div className="flex justify-center my-2 ">
+                <span className="border-[4px] border-green-500 rounded-full p-3 mx-1 mb-3">
+                  <FaRegUser className="text-[50px]" />
+                </span>
+              </div>
+              <div className="flex flex-col items-center ">
+                <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                  <FaRegEnvelope className="text-gray-400 mr-2" />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                    className="bg-gray-100 outline-none text-sm flex-1"
+                  />
+                </div>
+                <div className="bg-gray-100 w-64 p-2 flex items-center mb-1">
+                  <MdLockOutline className="text-gray-400 mr-2" />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                    className="bg-gray-100 outline-none text-sm flex-1"
+                  />
+                </div>
+                <div className="flex justify-between w-64 mb-5">
+                  <a href="#" className="text-xs text-gray-400">
+                    Forgot Password?
+                  </a>
+                </div>
+                <a
+                  href="#"
+                  className="border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white"
+                >
+                  Sign In
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-2/5 bg-green-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
+            <h2 className="text-3xl font-bold mb-2 ">Hello, Friend!</h2>
+            <div className="border-2 w-10 border-white inline-block mb-2"></div>
+            <p className="mb-10">
+              Fill up personal information and start journey with us.
+            </p>
+            <a
+              href="#"
+              className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-green-500"
+            >
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
